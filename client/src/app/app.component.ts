f9de020e21f6d3d2950c4e1787182c0c2c9759b8
ToNoMicro Denis <tonomicro@outlook.com>
2023-08-13 11:38:40 -0300

    final da parte 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient, private accountService: AccountService) {}
  
   ngOnInit(): void {
-    this.getUsers();
+   // this.getUsers();
     this.setCurrentUser();
   }
 
