1a71173805f8432ede253a47cea10532f07c08e1
ToNoMicro Denis <tonomicro@outlook.com>
2023-08-13 00:31:04 -0300

    só vai

--- API/Extentions/ApplicationServiceExtensions.cs
+++ API/Extentions/ApplicationServiceExtensions.cs
@@ -15,6 +15,9 @@ namespace API.Extentions
         });
         services.AddCors();
         services.AddScoped<ITokenService, TokenServices>();
+        services.AddScoped<IUserRepository, UserRepository>();
+
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
         return services;
         }
